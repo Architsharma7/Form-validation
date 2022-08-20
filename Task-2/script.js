9fbab5362e936fb1b6cbbc86ca2e013f5cf4a648
Archit Sharma <90101251+Architsharma7@users.noreply.github.com>
2022-08-20 05:43:11 +0530

    Update script.js

--- Task-2/script.js
+++ Task-2/script.js
@@ -1,31 +1,6 @@
 let tableFromJson = () => {
     //the json data.
-    const data = [{
-        "id": "0001",
-        "type": "donut",
-        "name": "Cake",
-        "ppu": 0.55,
-        "batters":
-            {
-                "batter":
-                    [
-                        { "id": "1001", "type": "Regular" },
-                        { "id": "1002", "type": "Chocolate" },
-                        { "id": "1003", "type": "Blueberry" },
-                        { "id": "1004", "type": "Devil's Food" }
-                    ]
-            },
-        "topping":
-            [
-                { "id": "5001", "type": "None" },
-                { "id": "5002", "type": "Glazed" },
-                { "id": "5005", "type": "Sugar" },
-                { "id": "5007", "type": "Powdered Sugar" },
-                { "id": "5006", "type": "Chocolate with Sprinkles" },
-                { "id": "5003", "type": "Chocolate" },
-                { "id": "5004", "type": "Maple" }
-            ]
-    }];   //json data comes inside these brackets BUT BECAUSE THE DATA PROVIDED IN FORM ISN'T CORRECT, I'M NOT ADDING IT HERE.
+    const data = [];   //json data comes inside these brackets BUT BECAUSE THE DATA PROVIDED IN FORM ISN'T CORRECT, I'M NOT ADDING IT HERE.
 
     let col = [];
     for (let i = 0; i < data.length; i++) {
